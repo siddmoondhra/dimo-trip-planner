58d2c087d2cccd93b41c5a16601dae0c9586499d
siddmoondhra <86398076+siddmoondhra@users.noreply.github.com>
2024-09-06 00:28:53 -0400

    Updating page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,71 +3,86 @@ import { useEffect, useState, ChangeEvent, FormEvent } from "react";
 import {
   APIProvider,
   Map,
-  useMapsLibrary,
-  useMap,
 } from "@vis.gl/react-google-maps";
+import { ApolloWrapper } from './apollo-client'; 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useLocation
+} from "react-router-dom";
 import "./styles.css";
 import Cookies from "js-cookie";
-import { useQuery, gql } from "@apollo/client";
-import location from '../components/data/location'
+import Location from '../components/data/Location'
+import Directions from "../components/Directions"
+import getUserFuelLevel from '../components/data/FuelLevel'
 
 
 
-// Define the type for the props of the Directions component
-interface DirectionsProps {
-  destinationInput: string;
-}
+
 
 var client_id = "0x1E0B0e5Aa4ABf2D2927639933d1324B6804538c3"
 
 export default function TripPlanner() {
+  
   /* 
-  Variables
+  Auth Flow
   */
-  
 
-
-
-  const position = {
-    lat: 37.2063119,
-    lng: -121.8620807,
-  };
-  const zoom = 10;
-
-  // State to store user input for destination
-  const [destinationInput, setDestinationInput] = useState<string>("");
-  // State to store the submitted input
-  const [submittedInput, setSubmittedInput] = useState<string>("");
-
-  console.log("START")
-  
   useEffect(() => {
     console.log("In USEEFFECT")
-    if (!readCookies()) {
+    if (isAuthNeeded()) {
+      console.log("in first section")
       Cookies.set("fetchAuth", "true")
+
+
       console.log("Cookies not found, going to FETCH")
       var base_url = "https://auth.dimo.zone/auth"
       var redirect_uri = "http://localhost:3000/"
       var extra_tail = "scope=openid email&response_type=code"
       var url = base_url+"?client_id="+client_id+"&redirect_uri="+redirect_uri+"&"+extra_tail
       console.log("FETCHing from "+url)
+      
       window.location.href = url
-      //Cookies.set("user", "authPassed", { expires: 14 });
-      //var accessToken = getAccessCode()
-      //console.log("after auth and fetch access code: "+accessToken)
-      //var tokenID = getTokenID()
-      console.log("getting access code")
-      getAccessCode()
-      //Cookies.set("access", accessToken, {expires: 14})
-      //Cookies.set("tokenID", tokenID, {expires: 14})
+   
+      
     } else {
-      console.log("Found cookies!")
+      var accessToken = getAccessTokenFromCookie()
+      console.log("access token from getacctokenfromcookie: " + accessToken)
+
+      if (accessToken == "UNKNOWN") {
+        console.log("in second section")
+        
+        console.log("Found cookies! getting access code")
+        getUserIDs()
+        
+      } else {
+        console.log("in third section") 
+
+        ///const accessToken = Cookies.get('accessToken'); 
+        getUserInfo()
+      }
     }
   }, []);
 
-  var tokenID = Cookies.get("tokenID")
-  ///console.log("access token:" + accessToken)
-  console.log("tokenID: " + tokenID)
+  /* 
+  Variables
+  */
+  //console.log("MAIN PAGE");
+  const [userLocation, setUserLocation] = useState<{ lat: number; lng: number } | null>(null);
+
+  // State to store user input for destination
+  const [destinationInput, setDestinationInput] = useState<string>("");
+  // State to store the submitted input
+  const [submittedInput, setSubmittedInput] = useState<string>("");
+
+  const position = {
+    lat: 37.2063119,
+    lng: -121.8620807,
+  };
+  const zoom = 10;
 
   // Function to handle changes in the input
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -80,19 +95,12 @@ export default function TripPlanner() {
     setSubmittedInput(destinationInput); // Update submitted input
   };
 
-
-  /* 
-  Main Return
-  */
+  //console.log("about to main return");
   return (
     <div style={{ height: "65vh", width: "100%", position: "relative" }}>
       <APIProvider apiKey={process.env.NEXT_PUBLIC_MAPS_API_KEY as string}>
-        <Map
-          defaultCenter={position}
-          defaultZoom={zoom}
-          mapId={process.env.NEXT_PUBLIC_MAP_ID}
-        >
-          <Directions destinationInput={submittedInput} />
+        <Map defaultCenter={position} defaultZoom={zoom} mapId={process.env.NEXT_PUBLIC_MAP_ID}>
+          <Directions origin="7089 Royal Ridge Dr, San Jose, CA" destination={submittedInput} />
           <Logout />
         </Map>
       </APIProvider>
@@ -110,86 +118,7 @@ export default function TripPlanner() {
         <button type="submit" style={{ color: "white" }}>
           Submit
         </button>
-
       </form>
-    </div>
-  );
-}
-
-/* 
-  Other Functions
-*/
-function Directions({ destinationInput }: DirectionsProps) {
-  // Access the map
-  const map = useMap();
-  // Dynamically load routes library
-  const position = {
-    lat: 37.2063119,
-    lng: -121.8620807,
-  };
-  const routesLibrary = useMapsLibrary("routes");
-  // Find directions then render them onto the map
-  const [directionsService, setDirectionsService] =
-    useState<google.maps.DirectionsService | null>(null);
-  const [directionsRenderer, setDirectionsRenderer] =
-    useState<google.maps.DirectionsRenderer | null>(null);
-  const [routes, setRoutes] = useState<google.maps.DirectionsRoute[]>([]);
-  const [routeIndex, setRouteIndex] = useState<number>(0);
-  // The selected route and leg within the route
-  const selected = routes[routeIndex];
-  const leg = selected?.legs[0];
-
-  useEffect(() => {
-    if (!routesLibrary || !map) return;
-    setDirectionsService(new routesLibrary.DirectionsService());
-    setDirectionsRenderer(new routesLibrary.DirectionsRenderer({ map }));
-  }, [routesLibrary, map]);
-
-  useEffect(() => {
-    if (!directionsRenderer || !directionsService || !destinationInput) return;
-
-    // Edit this: origin should be user location, destination should be user input
-    // and we can use dimo api to plan routes to other services like gas or tire pump
-    directionsService
-      .route({
-        origin: position,
-        destination: destinationInput, // Using the user input as the destination
-        travelMode: google.maps.TravelMode.DRIVING,
-        provideRouteAlternatives: true,
-      })
-      .then((response) => {
-        directionsRenderer.setDirections(response);
-        setRoutes(response.routes);
-      })
-      .catch((error) => {
-        console.error("Error fetching directions:", error);
-      });
-  }, [directionsService, directionsRenderer, destinationInput]);
-
-  useEffect(() => {
-    if (!directionsRenderer) return;
-    directionsRenderer.setRouteIndex(routeIndex);
-  }, [routeIndex, directionsRenderer]);
-
-  if (!leg) return null;
-  return (
-    <div className="directions">
-      <h2>{selected.summary}</h2>
-      <p>
-        {leg.start_address.split(",")[0]} to {leg.end_address.split(",")[0]}
-      </p>
-      <p>Distance: {leg.distance?.text}</p>
-      <p>Total Duration: {leg.duration?.text}</p>
-      <h2>Routes Available:</h2>
-      <ul>
-        {routes.map((route, index) => (
-          <li key={route.summary}>
-            <button onClick={() => setRouteIndex(index)}>
-              {route.summary}
-            </button>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
@@ -197,10 +126,10 @@ function Directions({ destinationInput }: DirectionsProps) {
 
 const Logout = () => {
   const handleOnClick = () => {
-    Cookies.remove("user");
     Cookies.remove("fetchAuth")
-    Cookies.remove("tokenID")
-    Cookies.remove("access")
+    Cookies.remove("accessToken")
+
+    //location.reload()
 
   };
   return (
@@ -210,146 +139,173 @@ const Logout = () => {
   );
 };
 
-function splitString(input: string): string {
-  // Find the index of '=' and '&'
-  const startIndex = input.indexOf("=") + 1;
-  const endIndex = input.indexOf("&");
-
-  // Extract the substring between '=' and '&'
-  if (startIndex > 0 && endIndex > startIndex) {
-    return input.substring(startIndex, endIndex);
-  } else {
-    return ""; // Return empty string if format is incorrect
-  }
-}
-
 // Check Cookies for dimo log in
-const readCookies = (): boolean => {
+const isAuthNeeded = (): boolean => {
+  console.log("checking for auth")
   const fetchAuth = Cookies.get("fetchAuth")
   if (fetchAuth !== undefined) {
     if (fetchAuth == "true") {
-      return true
+      return false
     }
   }
-    const user = Cookies.get("user");
-    const accessToken = Cookies.get("access")
-    //const tokenID = Cookies.get("tokenID")
-    console.log("Access from readCookies: '"+(accessToken as string)+"'")
-    console.log("User from readCookies: "+user)
-    //console.log("Token ID from readCookies: '"+ (tokenID as string)+"'")
-    if ((user == undefined) || (accessToken == undefined)) {
-      return false
-    }
-    if ((user == '') || (accessToken == '')) {
-      return false
-    }
-    return true
+  return true
 };
 
-function delay(ms: number) {
-  return new Promise( resolve => setTimeout(resolve, ms) );
-}
-//get access code for api calls
-function getAccessCode(){
-  var accessToken = ""
-  const userUrl = window.location.href;
-  const code = splitString(userUrl);
-  const data = new URLSearchParams({
-    client_id: client_id,
-    grant_type: "authorization_code",
-    code: code,
-    redirect_uri: "http://localhost:3000/",
-    }
-  );
-
-
-  var formBody = [];
-  var key = encodeURIComponent("client_id")
-  var value = encodeURIComponent(client_id)
-  formBody.push(key+"="+value)
-  key = encodeURIComponent("grant_type")
-  value = encodeURIComponent("authorization_code")
-  formBody.push(key+"="+value)
-  key = encodeURIComponent("code")
-  value = encodeURIComponent(code)
-  formBody.push(key+"="+value)
-  key = encodeURIComponent("redirect_uri")
-  value = encodeURIComponent("http://localhost:3000")
-  formBody.push(key+"="+value)
-  
-  const formB = formBody.join("&");  
-
-  console.log("found user_url: "+ userUrl+" code: "+code+" data: "+data)
-  console.log("running fetch for access code NOT ACCESSING")
-  fetch("https://auth.dev.dimo.zone/token", {
-    method: "POST",
-    body: formB,
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}
-  }).then((response) => {
-        console.log("IN RESPONSE")
-        console.log(response)
-        if (!response.ok) {
-          throw new Error("Network response was not ok " + response.statusText);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        accessToken = data.access_token;
-        console.log("Access Token:", accessToken);
-        Cookies.set("access", accessToken)
-        Cookies.set("user", "authPassed")
-        Cookies.set("tokenID", data.id_token)
-        return accessToken
-      })
-      .catch((error) => {
-        console.log("ERROR! ERROR!")
-        console.error("Error:", error);
-      });
-
-       delay(15000)
-  console.log("After fetch")
+const getAccessTokenFromCookie = (): string => {
+  const accessToken = Cookies.get("accessToken")
+  if (accessToken !== undefined) {
+      return accessToken
+  }
+  return "UNKNOWN"
 }
 
 
 
+function getUserIDs(): void {
+  console.log("in get user ids")
+  const urlParams = new URLSearchParams(window.location.search);
+  const myParam = urlParams.get("code");
+  console.log("got code, myParam = "+myParam)
 
+  interface TokenResponse {
+    access_token: string;
+    id_token: number;
+    expires_in: number;
+    refresh_token?: string;
+  }
 
-/*
-function getTokenID(): string{
-  var tokenID = ""
-    const userUrl = window.location.href;
-    const code = splitString(userUrl);
-    const data = new URLSearchParams({
-      client_id: process.env.REACT_APP_CLIENT_ID as string,
-      grant_type: "authorization_code",
-      code: code,
-      redirect_uri: "http://localhost:3000/",
-    });
+  if (myParam) {
+    fetch('https://auth.dimo.zone/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        'client_id': '0x1E0B0e5Aa4ABf2D2927639933d1324B6804538c3',
+        'grant_type': 'authorization_code',
+        'code': myParam,
+        'redirect_uri': 'http://localhost:3000/',
 
-    fetch("https://auth.dev.dimo.zone/token", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+      
+      })
     })
       .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok " + response.statusText);
-        }
+        console.log("GOT RESPONSE");
         return response.json();
       })
-      .then((data) => {
-       tokenID = data.id_token;
-        console.log("Token ID:", tokenID);
+      .then((data: TokenResponse) => { 
+        console.log("GOT DATA");
+        console.log(data);
+        const accessToken = data.access_token;
+        const tokenId = data.id_token
+        console.log("Access Token", accessToken);
+        Cookies.set("accessToken", accessToken)
+        ///Cookies.set("tokenId", (tokenId as unknown as string))
+        console.log("going to get user info")
+        getUserInfo()
       })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-
-
-   return tokenID;  
+      .catch((error) => console.error('Error:', error));
+  }
+}
 
 
 
+function getUserInfo(): void{
+  const accessToken = Cookies.get("accessToken");
+  fetch('https://users-api.dimo.zone/v1/user',{
+    method: 'get',
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
 
-   
-}*/
+    }
+  })
+  .then((response) => {
+    console.log("GOT RESPONSE FROM USERS API");
+    return response.json();
+  }).then((data) => {
+    console.log(data);
+    if (data.web3 && data.web3.address) {
+      console.log("getting token id")
+      getVehicleTokenId(data.web3.address);
+    }
+  }).catch((error) => console.error('Error:', error));
 
+}
+
+function getVehicleTokenId(wallet: string): void {
+  console.log("got to getVehicleTokenId");
+
+  fetch('https://identity-api.dimo.zone/query', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `{
+        vehicles(first: 1, filterBy: {owner: "${wallet}"}) {
+          nodes {
+            tokenId
+          }
+        }
+      }`
+    })
+  })
+  .then((response) => {
+    console.log("GOT RESPONSE FROM IDENTITY API");
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+
+    // Safely checking for the presence of vehicles and nodes
+    const vehicles = data?.data?.vehicles;
+    if (vehicles && vehicles.nodes && vehicles.nodes.length > 0) {
+      const tokenId = vehicles.nodes[0].tokenId;
+      console.log("Extracted tokenId:", tokenId);
+      // Pass tokenId to another function
+      getPrivToken(tokenId)
+    } else {
+      console.error("Vehicles or nodes not found in the response.");
+    }
+  })
+  .catch((error) => console.error('Error:', error));
+}
+
+
+function getPrivToken(tokenId: number) {
+  const accessToken = Cookies.get("accessToken");
+  console.log("Token ID inside getPrivToken:", tokenId); // Log tokenId for debugging
+  console.log("tokenid type: "+ typeof(tokenId))
+  if (!accessToken || !tokenId) {
+    console.error('Access Token or Token ID is missing or invalid');
+    return;
+  }
+
+  fetch('https://token-exchange-api.dimo.zone/v1/tokens/exchange', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+      'Content-Type': 'application/json', // Ensure correct Content-Type
+    },
+    body: JSON.stringify({
+      nftContractAddress: '0xbA5738a18d83D41847dfFbDC6101d37C69c9B0cF',
+      privileges: [1,2,3,4], // Example: 3 for current location
+      tokenId: tokenId // Ensure this is sent as an integer
+    })
+  })
+  .then((response) => {
+    console.log("GOT RESPONSE FROM TOKEN EXCHANGE");
+    if (!response.ok) {
+      console.log(response)
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    const privToken = data.token
+    console.log('privToken: '+privToken)
+    Location(tokenId,privToken)
+    ///getUserFuelLevel(tokenId, privToken)
+  })
+  .catch((error) => console.error('Error:', error));
+}
